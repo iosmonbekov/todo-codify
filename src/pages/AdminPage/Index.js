4c0ca494c778d5f96dfafa6de833afbe3cfdb03f
Dmitriy <shulzhenko.dm@gmail.com>
2022-09-12 19:07:31 +0600

    added array methods and API service

--- src/pages/AdminPage/Index.js
+++ src/pages/AdminPage/Index.js
@@ -1,34 +1,32 @@
-import { useState, useEffect } from 'react';
-import { deleteProduct, getProducts } from '../../api/api';
-import ConfirmModal from '../../components/ConfirmModal';
-import Product from './components/Product';
-import './admin-page.css'
+import { useState, useEffect } from "react";
+import { deleteProduct, getProducts } from "../../api/api";
+import ConfirmModal from "../../components/ConfirmModal";
+import Product from "./components/Product";
+import "./admin-page.css";
 
 function AdminPage() {
   const [list, setList] = useState([]);
   const [activeProduct, setActiveProduct] = useState(null);
 
   const handleDelete = async () => {
-    await deleteProduct(activeProduct.id)
-    setActiveProduct(null)
+    await deleteProduct(activeProduct.id);
+    setActiveProduct(null);
   };
 
   const onAction = (product) => {
     setActiveProduct(product);
-  }
+  };
   const fetch = async () => {
-    const products = await getProducts()
+    const products = await getProducts();
     setList(products);
-
-  }
+  };
 
   useEffect(() => {
-    fetch()
+    fetch();
   }, [activeProduct]);
 
-
   return (
-    <div className='admin-product-list'>
+    <div className="admin-product-list">
       <ul>
         {list?.map((product) => (
           <Product key={product.id} product={product} onAction={onAction} />
@@ -37,8 +35,8 @@ function AdminPage() {
       <ConfirmModal
         active={!!activeProduct}
         close={() => setActiveProduct(null)}
-        title={'Confirm deletion'}
-        buttonText={'Confirm'}
+        title={"Confirm deletion"}
+        buttonText={"Confirm"}
         callBack={handleDelete}
       />
     </div>
--- src/pages/HomePage/components/ProductList.js
+++ src/pages/HomePage/components/ProductList.js
@@ -1,31 +1,59 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Product from './Product';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Product from "./Product";
+import { getProducts } from "../../../api/api";
+import Array from "../../../array";
+import Modal from "./modal";
+import axios from "axios";
 
 function ProductList() {
   const [list, setList] = useState([]);
+  const [isShown, setIsShown] = useState(true);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const getProducts = async () => {
-    const response = await fetch(
-      'https://e-commerce-server-codify.herokuapp.com/products',
-      { method: 'GET' }
-    );
-    const products = await response.json();
-
+  const fetch = async () => {
+    const products = await getProducts();
     setList(products);
   };
 
+  useEffect(() => {
+    fetch();
+  }, []);
+
   const redirectToProduct = (id) => {
-    navigate(`product/${id}`);
+    navigate(`product/${id}`, { state: list });
+  };
+
+  const array = [1, [1, 2, [4, [5, 3]]], [22, [[4, 2], 1], 4], 5];
+
+  console.log(array.flat(3));
+
+  const log = () => {
+    console.log("AAAAAAA");
+  };
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://afisha-project.herokuapp.com/chapter/"
+    );
+    const res = await response.data;
+    console.log(res);
+  };
+  const fetchDataDetail = async (id) => {
+    const response = await axios.get(
+      `https://afisha-project.herokuapp.com/chapter/${id}/`
+    );
+    const res = await response.data;
+    console.log(res);
   };
 
   return (
-    <div className='product-list'>
+    <div className="product-list">
+      {/* <Array /> */}
+      <button onClick={() => setIsShown(true)}>ShowModal</button>
+      <Modal isShown={isShown} setIsShown={setIsShown} onSubmit={log}>
+        <p>Показать крик в консоли?</p>
+      </Modal>
       {list.map((product) => (
         <Product
           key={product.id}
