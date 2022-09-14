bb4f8b463ff19e3158e056e2a5ce6957473414ac
Dmitriy <shulzhenko.dm@gmail.com>
2022-09-14 19:44:51 +0600

    put

--- src/pages/AdminPage/components/Product.js
+++ src/pages/AdminPage/components/Product.js
@@ -1,15 +1,28 @@
-function Product({ product, onAction }) {
-  return (
-    <li className="product" onClick={() => console.log("CLICK")}>
-      <div className="product-info">
-        <p className="product-name">{product.name}</p>
-        <span className="product-price">${product.price}</span>
-      </div>
+import { useState } from "react";
+import AddModalUseForm from "../../../components/add-modal-useForm/add-modal";
 
-      <div className="buttons">
-        <button onClick={() => onAction(product)}>x</button>
-      </div>
-    </li>
+function Product({ product, onAction }) {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <>
+      <li className="product" onClick={() => console.log("CLICK")}>
+        <div className="product-info">
+          <p className="product-name">{product.name}</p>
+          <span className="product-price">${product.price}</span>
+        </div>
+
+        <div className="buttons">
+          <button onClick={() => setIsShown(true)}>Edit</button>
+          <button onClick={() => onAction(product)}>x</button>
+        </div>
+      </li>
+      <AddModalUseForm
+        isShown={isShown}
+        setIsshown={setIsShown}
+        product={product}
+      />
+    </>
   );
 }
 
