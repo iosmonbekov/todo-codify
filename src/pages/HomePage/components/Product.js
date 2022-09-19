a5d6c4a26630251b8f3c7dba58f522b6356829b7
Emil02072002 <isaev.aza.2000@gmail.com>
2022-09-19 19:33:08 +0600

    asdddd

--- src/pages/HomePage/components/Product.js
+++ src/pages/HomePage/components/Product.js
@@ -2,13 +2,10 @@ import { useDispatch } from "react-redux";
 import { addToCart } from "../../../store/actions/actions";
 
 function Product({ product, redirect }) {
-<<<<<<< HEAD
 const dispatch = useDispatch();
 dispatch()
 
-=======
   const dispatch = useDispatch();
->>>>>>> f79ba87509065b033dfe3afa82e4c770cbf118da
   return (
     <div onClick={() => redirect(product.id)} className="product">
       <div className="product-img">
